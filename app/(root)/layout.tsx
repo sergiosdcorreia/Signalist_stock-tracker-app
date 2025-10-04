4ad59dae3f26a8af550e3de3332c830a38ec0650
Sérgio Correia <sergiosdcorreia@gmail.com>
2025-10-04 16:41:03 +0100

    Header and desktop nav items

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {ReactNode} from "react";
+import Header from "@/components/Header";
 
-const Layout:({children}: {children: React.ReactNode}) => Element = ({children}: {children: React.ReactNode}) => {
+const Layout:({children}: {children: ReactNode}) => React.JSX.Element = ({children}: {children: React.ReactNode}) => {
     return (
         <main className="min-h-screen text-gray-400">
-            {/*Header*/}
+            <Header />
             <div className="container py-10">
                 {children}
             </div>
