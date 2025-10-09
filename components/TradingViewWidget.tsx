ee11983da8b5efe3f50aca5961ecfd55103314e8
Sergio Correia <sergiosdcorreia@gmail.com>
2025-10-09 22:24:24 +0100

    Implement home page with TradingView widgets

--- components/TradingViewWidget.tsx
+++ components/TradingViewWidget.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import useTradingViewWidget from "@/hooks/useTradingViewWidget";
-import React, { useRef, memo } from "react";
+import { cn } from "@/lib/utils";
+import React, { memo } from "react";
 
 interface TradingViewWidgetProps {
   title?: string;
@@ -18,27 +19,21 @@ const TradingViewWidget = ({
   height = 600,
   className,
 }: TradingViewWidgetProps) => {
-  const container = useTradingViewWidget(scriptUrl, config, height);
+  const containerRef = useTradingViewWidget(scriptUrl, config, height);
 
   return (
-    <div
-      className="tradingview-widget-container"
-      ref={container}
-      style={{ height: "100%", width: "100%" }}
-    >
+    <div className="w-full">
+      {title && (
+        <h3 className="font-semibold text-2xl text-gray-100 mb-5">{title}</h3>
+      )}
       <div
-        className="tradingview-widget-container__widget"
-        style={{ height: "calc(100% - 32px)", width: "100%" }}
-      ></div>
-      <div className="tradingview-widget-copyright">
-        <a
-          href="https://www.tradingview.com/symbols/NASDAQ-AAPL/"
-          rel="noopener nofollow"
-          target="_blank"
-        >
-          <span className="blue-text">AAPL stock chart</span>
-        </a>
-        <span className="trademark"> by TradingView</span>
+        className={cn("tradingview-widget-container", className)}
+        ref={containerRef}
+      >
+        <div
+          className="tradingview-widget-container__widget"
+          style={{ height, width: "100%" }}
+        ></div>
       </div>
     </div>
   );
